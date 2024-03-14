326cb689e3997dd10b9e2d8cdcdbafea2cea4129
a_koshmanov08@mail.ru <a_koshmanov08@mail.ru>
2024-03-14 15:06:59 +0400

    remade

--- MovieTheatre/src/routes/movie/+page.svelte
+++ MovieTheatre/src/routes/movie/+page.svelte
@@ -1,7 +1,7 @@
 <style>
     :root {
-    --primary-color: rgb(155, 213, 252);
-    --secodary-color: rgb(103, 170, 247);
+    --primary-color: rgb(41, 52, 39);
+    --secodary-color: rgb(26, 217, 255);
 }
 
 * {
@@ -45,8 +45,8 @@ header {
 }
 
 .movie {
-    width: 300px;
-    margin: 10px;
+    width: 3000px;
+    margin: 30px;
     background-color: var(--secodary-color);
     box-shadow: 0 4px 5px rgba(0, 0, 0, 0.2);
     position: relative;
@@ -63,7 +63,7 @@ header {
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding: 20px 30px 30px;
+    padding: 40px 30px 30px;
     letter-spacing: 0.5px;
 }
 
@@ -84,8 +84,9 @@ header {
 
 .overview {
     background-color: white;
-    padding: 20px;
+    padding: 7px;
     position: absolute;
+    text-decoration-color: aliceblue;
     left: 0;
     bottom: 0;
     right: 0;
@@ -102,20 +103,57 @@ header {
 <main id="main">
     <div class="movie" id="movie">
         <div class="movie-info" id="MovieInfoDiv">
-            <h3 id="MovieTitle">Крепкий Орешек</h3>
+            <h3 id="MovieTitle">Адам и Ева</h3>
             <span class="green">9.8</span>
         </div>
         <div class="overview">
-            <h3 id="Description">Описание фильма</h3>
+            <h3 id="Description">Романтическая комедия, Марина и Виктор - остались единственными людьми в огромном городе...</h3>
         </div>
     </div>
     <div class="movie" id="movie">
         <div class="movie-info" id="MovieInfoDiv">
-            <h3 id="MovieTitle">Не Крепкий Орешек</h3>
+            <h3 id="MovieTitle">Первая дуэль</h3>
             <span class="green">8.0</span>
         </div>
         <div class="overview">
-            <h3 id="Description">Описание фильма</h3>
+            <h3 id="Description">Драма, исторически, Экшен; Париж 1887 год. Время когда дороже всего ценилась дуэль...</h3>
         </div>
     </div>
+    <div class="movie" id="movie">
+        <div class="movie-info" id="MovieInfoDiv">
+            <h3 id="MovieTitle">Демоны дома Гарретов</h3>
+            <span class="green">7.9</span>
+        </div>
+        <div class="overview">
+            <h3 id="Description">Хоррор, триллер: Джой - Неле гальная иммигрантка, которая берётся...  </h3>
+        </div>
+    </div>
+    <div class="movie" id="movie">
+        <div class="movie-info" id="MovieInfoDiv">
+            <h3 id="MovieTitle">Лёд 3</h3>
+            <span class="green">9.0</span>
+        </div>
+        <div class="overview">
+            <h3 id="Description">Мелодрама: Надя выросла и стала фигуристкой. Она мечтает о... </h3>
+        </div>
+    </div>
+    <div class="movie" id="movie">
+        <div class="movie-info" id="MovieInfoDiv">
+            <h3 id="MovieTitle">Позывной "Пассажир"</h3>
+            <span class="green">9.7</span>
+        </div>
+        <div class="overview">
+            <h3 id="Description">Драмма: Молодой писатель живёт в столице, проводит время в... </h3>
+        </div>
+    </div>
+    <div class="movie" id="movie">
+        <div class="movie-info" id="MovieInfoDiv">
+            <h3 id="MovieTitle">Валентина</h3>
+            <span class="green">7.0</span>
+        </div>
+        <div class="overview">
+            <h3 id="Description">Анимация: Девочка хочет стать гимнасткой и для этого... </h3>
+        </div>
+    </div>
+
 </main>
